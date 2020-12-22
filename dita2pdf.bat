c5bc1404105b1f39ec5af084fbee50c45c922b92
Francis ANDRE <francis.andre.kampbell@orange.fr>
2020-12-22 14:48:16 +0100

    restore echo off

--- dita2pdf.bat
+++ dita2pdf.bat
@@ -1,4 +1,4 @@
-rem echo off
+echo off
 setlocal
 set DITA=C:\DITA\dita-ot-3.3.3\bin\dita.bat
 rem for /D %%i in (pdfs\out\M*) do  %DITA% -i %%i\MIS_3287_Connector_PG_4.3.ditamap -f pdf -o pdfs\pdf\%%~ni.pdf
